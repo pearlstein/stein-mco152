3087e415b6d8a069a0ce6f2e66c36ca9a1f5e7a1
Penina <Penina@192.168.1.3>
2013-03-10 22:14:08 -0400

    duplicateInteger commit

--- src/stein/interviews/DuplicateInteger.java
+++ src/stein/interviews/DuplicateInteger.java
@@ -6,18 +6,40 @@ public class DuplicateInteger {
 		int low=0;
 		int mid=0;
 	
-		while(low<high){
+		while(low<=high){
 			mid=(low+high)/2;
-			if(low==mid||high==mid){
-				return array[mid];	
-			}
+			
 			if(array[mid]!=array[low]+mid-low){
-				high=mid;
+				high=mid-1;
 			}else{
-				low=mid;
+				low=mid+1;
 			}
+		}
+		
+		if(low==mid||high==mid){
+			return array[mid];	
 		}
 		return -1;
 	}
-
+public static void main(String[] args){
+	DuplicateInteger x=new DuplicateInteger();
+	
+	int[] array=new int[12];
+	
+	array[0]=1;
+	array[1]=2;
+	array[2]=3;
+	array[3]=4;
+	array[4]=5;
+	array[5]=6;
+	array[6]=7;
+	array[7]=8;
+	array[8]=9;
+	array[9]=10;
+	array[10]=10;
+	array[11]=11;
+	
+	int num=x.findDuplicate(array);
+	System.out.println(num);
+}
 }
